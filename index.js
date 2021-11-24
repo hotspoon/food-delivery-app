f6440609d5f31f104234721fb87a5834fda72d84
faris18 <fariz@gdc.co.id>
2021-11-24 12:08:31 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+// function myFunction() {
+//   var x = document.getElementById("navigation");
+//   if (x.className === "navigation") {
+//     x.className += " navigation__responsive";
+//   } else {
+//     x.className = "navigation";
+//   }
+// }
